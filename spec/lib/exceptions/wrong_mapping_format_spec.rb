d737b01e2381a05b3256a98cb2a42319b90a98e7
Ignat Zakrevsky <iezakrevsky@gmail.com>
2015-04-03 00:29:37 +0300

    Specs for WrongMappingFormat

---
+++ spec/lib/exceptions/wrong_mapping_format_spec.rb
@@ -0,0 +1,9 @@
+require_relative "../../spec_helper"
+require_relative "../../../lib/exceptions/wrong_mapping_format"
+
+describe Cascade::WrongMappingFormat do
+  subject { Cascade::WrongMappingFormat.new }
+  it 'exception raisable' do
+    assert_respond_to subject, :exception
+  end
+end
