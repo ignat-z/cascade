3a9871db83b86198a345e48a46d6b9d7516b2ced
Ignat Zakrevsky <iezakrevsky@gmail.com>
2015-04-06 23:58:17 +0300

    Optional MonkeyPatching for #reverse_merge

--- lib/cascade/helpers/hash.rb
+++ lib/cascade/helpers/hash.rb
@@ -1,5 +1,9 @@
 class Hash
-  def reverse_merge(other_hash)
-    other_hash.merge(self)
+  unless Hash.instance_methods.include? :reverse_merge
+    # Merges the caller into +other_hash+
+    #
+    def reverse_merge(other_hash)
+      other_hash.merge(self)
+    end
   end
 end
