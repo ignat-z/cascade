63ad7aeccd4f23e3624764f9188e96793ec57638
Ignat Zakrevsky <iezakrevsky@gmail.com>
2015-04-16 02:00:23 +0300

    Parallelize for MiniTests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,10 +7,15 @@ end
 require "minitest/autorun"
 require "minitest/pride"
 require "minitest/mock"
+require "minitest/hell"
 require "shoulda/matchers"
 require "rr"
 
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+
+class Minitest::Test
+  parallelize_me!
+end
 
 alias :context :describe
